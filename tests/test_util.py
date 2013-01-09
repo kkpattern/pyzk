e90df77eea90748a826e4cfb7fdb6028e0fcb79b
kylerzhang <kylerzhang11@gmail.com>
2013-01-09 16:54:36 +0800

    add Retry test.

--- tests/test_util.py
+++ tests/test_util.py
@@ -22,3 +22,26 @@ class TestProgress(unittest.TestCase):
         progress = pyzk.util.Progress(current_value=1.1, max_value=100)
         assert float(progress)*100 == 1.1
         assert str(progress) == r"1.1%"
+
+class MockExceptionHandler(object):
+    def __init__(self):
+        self.call_count = 0
+    def __call__(self, a, b):
+        self.call_count += 1
+
+def mock_target():
+    raise IOError("Mock IOError")
+
+class TestRetry(unittest.TestCase):
+    def test_basic(self):
+        MAX_RETRY = 6
+        mock_exception_handler = MockExceptionHandler()
+        target_with_retry = pyzk.util.Retry(
+            mock_target,
+            MAX_RETRY,
+            mock_exception_handler)
+        try:
+            target_with_retry()
+        except IOError:
+            pass
+        assert mock_exception_handler.call_count == MAX_RETRY-1
