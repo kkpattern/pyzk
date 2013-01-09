2270a74096f9f7ce48c9b60ec89cb8dad5132ca7
kylerzhang <kylerzhang11@gmail.com>
2013-01-09 16:02:25 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import sys
+from distutils.core import setup
+
+if sys.version_info[0] < 3:
+    print("Need python version >=3.0")
+    exit(1)
+
+setup(name="pyzk",
+      version="0.0.1",
+      description="The personal python lib of zhangkai.",
+      author="Zhang Kai",
+      author_email="kylerzhang11@gmail.com",
+      packages=["pyzk"],
+      )
+
