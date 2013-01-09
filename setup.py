bbc5f0fb5afa990528f7aeb64478847c404698df
kylerzhang <kylerzhang11@gmail.com>
2013-01-09 16:15:26 +0800

    remove python>=3.0 requirement.

--- setup.py
+++ setup.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python
 
-import sys
 from distutils.core import setup
 
-if sys.version_info[0] < 3:
-    print("Need python version >=3.0")
-    exit(1)
 
 setup(name="pyzk",
       version="0.0.1",
